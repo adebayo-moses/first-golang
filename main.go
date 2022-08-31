863ecbe998845352d93e35030ad6f0ac8dd042b6
Adebayo Moses <adebayomoses597@gmail.com>
2022-08-31 17:05:05 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(" Hello Moses!")
+}
+
+// package main
+
+// import "fmt"
+
+// // var someName = "Hellos" !
+
+// func main() {
+// 	var nameOne string = "moses"
+// 	var nameTwo = "max"
+// 	var nameThree string
+
+// 	fmt.Println(nameOne, nameTwo, nameThree)
+
+// 	nameOne = "eminent"
+// 	nameThree = "timi"
+
+// 	fmt.Println(nameOne, nameTwo, nameThree)
+
+// 	nameFour := "yoshi"
+// 	fmt.Println(nameFour)
+// }
+
+package main
+import "fmt"
+
+func main() {}
+
+var ageOne int = 20
+var ageTwo = 30
+ageThree := 40
+
+fmt.Println(ageOne, ageTwo, ageThree)
+
+// //bits & memory
+// var numOne int8 = 25
+// var numTwo int8 = -125
+// varr numThree unit = -25
+
+var scoreOne float32 = +25.98
+var scoreTwo float64 = 898888989898989778777877.7
+scoreThree := 1.5
